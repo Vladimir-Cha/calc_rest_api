c7cdd11a231e249f3949c14609de18e05d6ccb11
Chaykovsky Vladimir <onlyone281@mail.ru>
2025-06-02 21:44:23 +0300

    Добавлен мидлваре для Echo, сохранение вычислений через sync.Mutex и ручка для умножения

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 /*Калькулятор работает в любом web-клиенте.
@@ -34,20 +36,27 @@ type SumOfNumbers struct {
 }
 
 type ResponseSumOfNumbers struct {
-	ResponseNumbers float64 `json:"sum"`
+	ResponseNumbers       float64 `json:"sum"`
+	MultiplicationNumbers float64 `json:"multipl"`
 }
+
+var (
+	totalSum float64
+	totalMul float64
+	mu       sync.Mutex // Переменная для mutex
+)
 
 // sumHandler - обработчик HTTP-запросов по пути /sum
 func sumHandler(c echo.Context) error {
 	//Если метод не POST, то выводим ошибку
-	if c.Request().Method != http.MethodPost {
+	/*if c.Request().Method != http.MethodPost {
 		return c.JSON(http.StatusMethodNotAllowed, "Метод должен быть POST")
 	}
-
+	*/
 	var req SumOfNumbers
 	//декодер для тела запроса
 	if err := c.Bind(&req); err != nil { //парсим JSON в структуру SumOfNumbers
-		return c.JSON(http.StatusBadRequest, "Неверный формат JSON")
+		return c.JSON(http.StatusBadRequest, "Неверный формат JSON"+err.Error())
 	}
 
 	//Считаем сумму чисел
@@ -56,18 +65,57 @@ func sumHandler(c echo.Context) error {
 		sum += num
 	}
 
+	mu.Lock()
+	totalSum += sum
+	mu.Unlock()
+
 	//Формируем ответ
 	//response := map[string]float64{"sum": sum} //мапа для ответа
 	return c.JSON(http.StatusOK, ResponseSumOfNumbers{ResponseNumbers: sum})
 
 }
 
+func getSumHandler(c echo.Context) error {
+	mu.Lock()
+	defer mu.Unlock()
+	return c.JSON(http.StatusOK, ResponseSumOfNumbers{ResponseNumbers: totalSum})
+}
+
+func multimplHandler(c echo.Context) error {
+	var req SumOfNumbers
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, "Неверный формат JSON"+err.Error())
+	}
+
+	var mul float64
+	mul = 1
+	for _, num := range req.Numbers {
+		mul *= num
+	}
+	mu.Lock()
+	totalMul += mul
+	mu.Unlock()
+	return c.JSON(http.StatusOK, ResponseSumOfNumbers{MultiplicationNumbers: mul})
+}
+
+func getMulHandler(c echo.Context) error {
+	mu.Lock()
+	defer mu.Unlock()
+	return c.JSON(http.StatusOK, ResponseSumOfNumbers{MultiplicationNumbers: totalMul})
+}
+
 func main() {
 	//Создаем сервер
 	e := echo.New()
 
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+
 	//Регистрируем маршрут
 	e.POST("/sum", sumHandler)
+	e.GET("/totalsum", getSumHandler)
+	e.POST("/mul", multimplHandler)
+	e.GET("/totalmul", getMulHandler)
 
 	fmt.Println("Запускаем сервер")
 
